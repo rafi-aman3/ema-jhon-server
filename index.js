cb3f750b4c8594b9a38bfb7f8b01a03268ed65e3
rafi-aman3 <rafiaman03@gmail.com>
2021-03-30 06:14:34 +0600

    port update

--- index.js
+++ index.js
@@ -69,6 +69,4 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`listening at http://localhost:${port}`)
-})
+app.listen(process.env.PORT || port)
